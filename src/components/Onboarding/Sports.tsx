e9d15a9147d3b74c0a05199c83dc8fa95d415798
Babatunde Isiaka <tundeloper@gmail.com>
2025-06-01 17:57:18 +0100

    verify

--- src/components/Onboarding/Sports.tsx
+++ src/components/Onboarding/Sports.tsx
@@ -2,10 +2,11 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import logo from "@/assets/logo2.png";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Checkbox from "../ui/checkedBox";
 import { useTheme } from "@/context/ThemeContext";
 import GradientButton from "../ui/gradientButton";
+import axios from "axios";
 
 const sportsList = [
   "Football",
@@ -17,9 +18,9 @@ const sportsList = [
 ];
 
 const Sports = () => {
-//   const router = useRouter();
+  const router = useRouter();
 
-  const {theme} = useTheme()
+  const { theme } = useTheme();
 
   const [selectedSports, setSelectedSports] = useState<string[]>([]);
 
@@ -29,20 +30,38 @@ const Sports = () => {
     );
   };
 
-    const handleNext = () => {
-      alert(`Selected sports: [${selectedSports.join(', ') || ''}]`);
-    };
+  const handleNext = async () => {
+    // alert(`Selected sports: [${selectedSports.join(', ') || ''}]`);
+    try {
+      const response = await axios.put(
+        "https://lazeapi-v2.onrender.com/api/preferences/sports",
+        {
+          favorite_sports: [selectedSports.join(', ') || ''],
+        }
+      );
+
+      if (response) {
+        router.push('/onbiarding/teams')
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   //   const handleSkip = () => {
   //     alert('Skipped');
   //   };
 
-//   const handleNext = () => {
-//     router.push("/onboarding/welcome");
-//   };
+  //   const handleNext = () => {
+  //     router.push("/onboarding/welcome");
+  //   };
 
   return (
-    <div className={`md:max-w-[550px] w-full bg-white ${theme === "dark" ? 'bg-[#121212]' : 'bg-white'} rounded flex flex-col items-center gap-1 p-8 md:shadow-card md:p-8`}>
+    <div
+      className={`md:max-w-[550px] w-full bg-white ${
+        theme === "dark" ? "bg-[#121212]" : "bg-white"
+      } rounded flex flex-col items-center gap-1 p-8 md:shadow-card md:p-8`}
+    >
       <div className="flex flex-col items-center gap-2 w-[427px] mb-7">
         <Image
           src={logo}
@@ -51,13 +70,18 @@ const Sports = () => {
           height={76}
           className="w-[75px] md:w-[114px] h-[50px] md:h-[76px]"
         />
-        <h1 className={`font-[500] text-[20px] md:text-[25px] font-switzer leading-[1.32em] text-center text-[#3A3D46] `}>
+        <h1
+          className={`font-[500] text-[20px] md:text-[25px] font-switzer leading-[1.32em] text-center text-[#3A3D46] `}
+        >
           What Sports do you love?
         </h1>
       </div>
       <div className="grid grid-cols-2 gap-y-2 gap-x-[180px] max-w-full mb-6 md:gap-x-[280px]">
         {sportsList.map((sport) => (
-          <label key={sport} className="flex items-center space-x-2 cursor-pointer">
+          <label
+            key={sport}
+            className="flex items-center space-x-2 cursor-pointer"
+          >
             {/* <input
               type="checkbox"
               checked={selectedSports.includes(sport)}
@@ -68,18 +92,22 @@ const Sports = () => {
               checked={selectedSports.includes(sport)}
               onChange={() => handleToggle(sport)}
             />
-            <p className={''}>{sport}</p>
+            <p className={""}>{sport}</p>
           </label>
         ))}
       </div>
       <div className="flex flex-row w-full gap-4 mt-7">
-        <GradientButton onClick={handleNext} className="flex-1 h-[50px] text-[16px] text-white hover:bg-[#2D439B]/80 transition-all duration-300 cursor-pointer font-switzer font-normal rounded shadow-md disabled:opacity-60 bg-[#2D439B] py-2 ">
-            Next
+        <GradientButton
+          onClick={handleNext}
+          className="flex-1 h-[50px] text-[16px] text-white hover:bg-[#2D439B]/80 transition-all duration-300 cursor-pointer font-switzer font-normal rounded shadow-md disabled:opacity-60 bg-[#2D439B] py-2 "
+        >
+          Next
         </GradientButton>
 
         <button
           className="flex-1 flex items-center justify-center h-[50px] bg-[#D9D9D9] hover:bg-[#D9D9D9]/80 transition-all duration-300 cursor-pointer font-switzer text-[#3A3D46] rounded shadow font-normal text-[16px] leading-[1.5em]"
           style={{ boxShadow: "0px 2px 0px 0px rgba(0,0,0,0.02)" }}
+          onClick={() => router.push('/onboarding/welcome')}
         >
           Skip
         </button>
