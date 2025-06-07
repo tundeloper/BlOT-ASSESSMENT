d04b423cbc6965c3e571846365e3b16c151ace18
Babatunde Isiaka <tundeloper@gmail.com>
2025-06-07 14:53:52 +0100

    google redirect

--- src/components/Auth/SignupForm.tsx
+++ src/components/Auth/SignupForm.tsx
@@ -426,7 +426,7 @@ const Signup = () => {
             const url =
               `https://accounts.google.com/o/oauth2/v2/auth?` +
               `client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}` +
-              `&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}` +
+              `&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}/register` +
               `&response_type=token` +
               `&scope=openid%20email%20profile` +
               `&response_mode=fragment`;
