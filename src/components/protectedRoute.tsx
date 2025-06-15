00ea966d0d03f806c570f59882155753b7720a3d
Babatunde Isiaka <tundeloper@gmail.com>
2025-06-15 17:32:16 +0100

    protetced route loading state

--- src/components/protectedRoute.tsx
+++ src/components/protectedRoute.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react'
 import { useAuthStore } from "@/store/authstore"
 import { useRouter } from "next/navigation"
+import { CircularProgress } from '@mui/material'
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const hasHydrated = useAuthStore((s) => s.hasHydrated)
@@ -15,8 +16,8 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
     }
   }, [hasHydrated, isAuthenticated, router])
 
-  if (!hasHydrated) return <div>Loading...</div>
-  if (!isAuthenticated) return <div className='flex h-screen items-center justify-center dark:text-white'>loading ...</div>
+  if (!hasHydrated) return <div className='flex h-screen items-center justify-center dark:text-white'><CircularProgress size={40} sx={{ color: '#2D439B' }} /></div>
+  if (!isAuthenticated) return <div className='flex h-screen items-center justify-center dark:text-white'><CircularProgress size={40} sx={{ color: '#2D439B' }} /></div>
 
   return <div className="">{children}</div>
 }
