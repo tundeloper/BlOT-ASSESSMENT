06851ecddd4ad7a713708340c8a44f1a04bbbdd3
Babatunde Isiaka <tundeloper@gmail.com>
2025-07-02 14:55:00 +0100

    comment

--- src/components/Feed/comments/CommentList.tsx
+++ src/components/Feed/comments/CommentList.tsx
@@ -6,7 +6,12 @@ import { Dispatch, SetStateAction, useState } from "react";
 import Image from "next/image";
 import { CircularProgress, ClickAwayListener } from "@mui/material";
 import CommentPopOver from "./commentpopover";
-import { createComment, deleteComment, likeComment, unlikeComment } from "@/api/post";
+import {
+  createComment,
+  deleteComment,
+  likeComment,
+  unlikeComment,
+} from "@/api/post";
 import formatTime from "@/utils/timeformater";
 import { useTheme } from "@/context/ThemeContext";
 import { enqueueSnackbar } from "notistack";
@@ -49,11 +54,10 @@ const CommentList: React.FC<CommentListProps> = ({ comments, setComments }) => {
   };
 
   const handleDelete = async () => {
-    const res = await deleteComment(comments.id)
-    console.log(res)
+    const res = await deleteComment(comments.id);
+    console.log(res);
     setComments((prev) => removeComment(prev, comments.id));
-  }
-
+  };
 
   const handleComment = async () => {
     // if (!isAuthenticated) {
@@ -136,9 +140,7 @@ const CommentList: React.FC<CommentListProps> = ({ comments, setComments }) => {
                   size={20}
                   className="text-[#3A3D46] dark:text-white"
                 />
-                <span
-                  className="text-[13px] text-[#3A3D46] dark:text-white"
-                >
+                <span className="text-[13px] text-[#3A3D46] dark:text-white">
                   {comments.replies.length}
                 </span>
               </button>
@@ -146,7 +148,7 @@ const CommentList: React.FC<CommentListProps> = ({ comments, setComments }) => {
               <button
                 className="flex items-end gap-0.5 cursor-pointer"
                 onClick={() => {
-                  setReplying(true);
+                  setReplying((prev) => !prev);
                 }}
               >
                 <BsReplyAll
@@ -177,7 +179,10 @@ const CommentList: React.FC<CommentListProps> = ({ comments, setComments }) => {
                       }}
                     >
                       <div className="absolute right- w-[140px] bg-white rounded shadow-lg overflow-hidden z-50">
-                        <CommentPopOver onClose={handleClose} handleDelete={handleDelete}  />
+                        <CommentPopOver
+                          onClose={handleClose}
+                          handleDelete={handleDelete}
+                        />
                       </div>
                     </ClickAwayListener>
                   )}
@@ -188,74 +193,87 @@ const CommentList: React.FC<CommentListProps> = ({ comments, setComments }) => {
               </label>
             </div>
             {replying && (
-              <div className="w-full mt-1">
-                <div className="flex justify-between w-full bg-[#E4E6EC] dark:bg-[#1A1C20] rounded-sm px-3 py-2 ">
-                  <p className="text-[12px] mdtext-[13px]">
-                    Replying to {comments.author_name}
-                  </p>
-                  <GiCancel
-                    className="text-[#3A3D46] dark:text-white cursor-pointer"
-                    onClick={() => {
-                      setReplying(false);
-                    }}
-                  />
-                </div>
-                <div className="flex justify-between items-center gap-2 w-full">
-                  <div className="flex items-center gap-2 w-full">
-                    <span className="text-[11px] md:text-[13px] ">
-                      @{comments.author_username}
-                    </span>
-                    <input
-                      type="text"
-                      placeholder="Write a comment"
-                      className="flex-1 bg-transparent text-[12px] text-[#3A3D46] dark:text-white outline-none placeholder:text-[#3A3D46] dark:placeholder:text-white h-[30px]"
-                      value={comment}
-                      onChange={(e) => setComment(e.target.value)}
-                      onFocus={() => setIsCommentFocused(true)}
-                    />
-                  </div>
-                  {isCommentFocused && (
-                    <button onClick={handleComment} className="cursor-pointer">
-                      {isCommenting ? (
-                        <CircularProgress
-                          size={20}
-                          sx={{ color: theme === "dark" ? "#fff" : "#3A3D46" }}
-                        />
-                      ) : (
-                        <AiOutlineSend
-                          size={20}
-                          className="text-[#3A3D46] dark:text-white"
-                        />
-                      )}
-                    </button>
-                  )}
-                </div>
-                {isCommentFocused && (
-                  <div className="flex justify-between w-full items-center">
-                    <div className="flex gap-4 items-center">
-                      <label htmlFor="image" className="cursor-pointer">
-                        <CiImageOn
-                          size={20}
-                          className="text-[#3A3D46] dark:text-white"
-                        />
-                      </label>
-                      <input type="file" id="image" className="hidden" />
-                      <button className="cursor-pointer">
-                        <HiOutlineGif
-                          size={20}
-                          className="text-[#3A3D46] dark:text-white"
-                        />
-                      </button>
-                      <button className="cursor-pointer">
-                        <BsEmojiSmile
-                          size={18}
-                          className="text-[#3A3D46] dark:text-white"
-                        />
-                      </button>
+              <ClickAwayListener
+                onClickAway={() => {
+                  setReplying(false);
+                }}
+              >
+                <>
+                  <div className="w-full mt-1">
+                    <div className="flex justify-between w-full bg-[#E4E6EC] dark:bg-[#1A1C20] rounded-sm px-3 py-2 ">
+                      <p className="text-[12px] mdtext-[13px]">
+                        Replying to {comments.author_name}
+                      </p>
+                      <GiCancel
+                        className="text-[#3A3D46] dark:text-white cursor-pointer"
+                        onClick={() => {
+                          setReplying(false);
+                        }}
+                      />
                     </div>
+                    <div className="flex justify-between items-center gap-2 w-full">
+                      <div className="flex items-center gap-2 w-full">
+                        <span className="text-[11px] md:text-[13px] ">
+                          @{comments.author_username}
+                        </span>
+                        <input
+                          type="text"
+                          placeholder="Write a comment"
+                          className="flex-1 bg-transparent text-[12px] text-[#3A3D46] dark:text-white outline-none placeholder:text-[#3A3D46] dark:placeholder:text-white h-[30px]"
+                          value={comment}
+                          onChange={(e) => setComment(e.target.value)}
+                          onFocus={() => setIsCommentFocused(true)}
+                        />
+                      </div>
+                      {isCommentFocused && (
+                        <button
+                          onClick={handleComment}
+                          className="cursor-pointer"
+                        >
+                          {isCommenting ? (
+                            <CircularProgress
+                              size={20}
+                              sx={{
+                                color: theme === "dark" ? "#fff" : "#3A3D46",
+                              }}
+                            />
+                          ) : (
+                            <AiOutlineSend
+                              size={20}
+                              className="text-[#3A3D46] dark:text-white"
+                            />
+                          )}
+                        </button>
+                      )}
+                    </div>
+                    {isCommentFocused && (
+                      <div className="flex justify-between w-full items-center">
+                        <div className="flex gap-4 items-center">
+                          <label htmlFor="image" className="cursor-pointer">
+                            <CiImageOn
+                              size={20}
+                              className="text-[#3A3D46] dark:text-white"
+                            />
+                          </label>
+                          <input type="file" id="image" className="hidden" />
+                          <button className="cursor-pointer">
+                            <HiOutlineGif
+                              size={20}
+                              className="text-[#3A3D46] dark:text-white"
+                            />
+                          </button>
+                          <button className="cursor-pointer">
+                            <BsEmojiSmile
+                              size={18}
+                              className="text-[#3A3D46] dark:text-white"
+                            />
+                          </button>
+                        </div>
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
+                </>
+              </ClickAwayListener>
             )}
           </div>
         </div>
