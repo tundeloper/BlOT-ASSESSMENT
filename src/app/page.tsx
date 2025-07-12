730f90037ffc451d3bcf58bfb1f7903a63b8138e
Babatunde Isiaka <tundeloper@gmail.com>
2025-07-12 05:08:45 +0100

    hhh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import HomeMain from "@/Components/HomeMain";
+// import HomeMain from "@/Components/HomeMain";
 
 export default function HomePage() {
   return (
       <div className="flex flex-col items-center justify-center h-full">
-        <HomeMain />
+        {/* <HomeMain /> */}
       </div>
   );
 }
