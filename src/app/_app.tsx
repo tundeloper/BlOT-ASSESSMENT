24eb2105f47745f5a406e7f69c889a846f88b099
Babatunde Isiaka <tundeloper@gmail.com>
2025-07-12 05:13:24 +0100

    h

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,11 +1,8 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@/context/ThemeContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log("App component rendered");
   return (
-    <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
   );
 }
